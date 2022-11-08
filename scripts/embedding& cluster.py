4b66a1ca339c224d26c3b516c5737703a8f81c42
sky <sl6246@nyu.edu>
2022-11-08 15:20:28 -0500

    adjusted for filtered sentences

--- scripts/embedding& cluster.py
+++ scripts/embedding& cluster.py
@@ -9,26 +9,26 @@ from sklearn.cluster import KMeans
 import sklearn.metrics as metrics
 
 nlp = spacy.load("en_core_web_sm")
-dataset = load_from_disk("/scratch/kd1860/DSGA_1006_capstone/dataset/scored_dataset_test500")
+dataset = load_from_disk("/scratch/kd1860/DSGA_1006_capstone/dataset/filtered_dataset")
 model = SentenceTransformer('distilbert-base-nli-mean-tokens')
 
+
 def get_clusters(document):
-    doc = nlp(document["document"]) #use spacy to get the document sentences
-
-    #bert embedding
+    # doc = nlp(document["document"]) #use spacy to get the document sentences
+    doc = document["document"]
+    # bert embedding
     embedding = []
-    for sentence in doc.sents:
-        embedding.append(model.encode(sentence.text, show_progress_bar=True))
+    for sentence in doc:
+        embedding.append(model.encode(sentence, show_progress_bar=True))
     document["embedding"] = embedding
-
 
     cur = -1
     best_label = np.ndarray(len(embedding))
-    cluster = np.arange(2,6)
+    cluster = np.arange(2, 6)
 
-    for i in range(2,6): # get best num_clusters from silhouette score
-        labels = KMeans(n_clusters=i).fit(embedding).labels_ #,init="k-means++",random_state=200
-        score = metrics.silhouette_score(embedding,labels,metric="euclidean")
+    for i in range(2, 6):  # get best num_clusters from silhouette score
+        labels = KMeans(n_clusters=i).fit(embedding).labels_  # ,init="k-means++",random_state=200
+        score = metrics.silhouette_score(embedding, labels, metric="euclidean")
         if score > cur:
             cur = score
             best_label = labels
