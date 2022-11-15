b519a13628e1ef21219effe911ba41785416df66
sky <sl6246@nyu.edu>
2022-11-15 16:11:30 -0500

    adjusted for filtered sentences

--- scripts/embedding.py
+++ scripts/embedding.py
@@ -13,7 +13,7 @@ dataset = load_from_disk("/scratch/kd1860/DSGA_1006_capstone/dataset/filtered_da
 model = SentenceTransformer('distilbert-base-nli-mean-tokens')
 
 
-def get_clusters(document):
+def get_embedding(document):
     # doc = nlp(document["document"]) #use spacy to get the document sentences
     doc = document["document"]
     # bert embedding
@@ -22,22 +22,9 @@ def get_clusters(document):
         embedding.append(model.encode(sentence, show_progress_bar=True))
     document["embedding"] = embedding
 
-    cur = -1
-    best_label = np.ndarray(len(embedding))
-    cluster = np.arange(2, 6)
-
-    for i in range(2, 6):  # get best num_clusters from silhouette score
-        labels = KMeans(n_clusters=i).fit(embedding).labels_  # ,init="k-means++",random_state=200
-        score = metrics.silhouette_score(embedding, labels, metric="euclidean")
-        if score > cur:
-            cur = score
-            best_label = labels
-
-    document["labels"] = best_label
     return document
 
 dataset = dataset.add_column("embedding", [[0]] * len(dataset))
-dataset = dataset.add_column("labels", [[0]] * len(dataset))
 
 start  = time.time()
 print('started')
@@ -47,6 +34,6 @@ end = time.time()
 print("map ended")
 print('save dataset')
 
-d.save_to_disk("/scratch/kd1860/DSGA_1006_capstone/dataset/clustered_dataset")
+d.save_to_disk("/scratch/kd1860/DSGA_1006_capstone/dataset/embedded_dataset")
 
 print (end-start)
