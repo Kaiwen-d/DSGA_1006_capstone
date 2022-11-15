334a9ed34740ded52697af56f6b6126244090066
Kaiwen Dai <kd1860@nyu.edu>
2022-11-15 16:55:23 -0500

    update select sentence

--- scripts/select_sentences.py
+++ scripts/select_sentences.py
@@ -9,15 +9,15 @@ nlp = spacy.load("en_core_web_sm")
 dataset = load_from_disk("/scratch/kd1860/DSGA_1006_capstone/dataset/scored_dataset_test500")
 
 def filter_sentence(document):
-  percentage = 0.8 #percentage to keep
-  top = 5 # keep first t sentences 
-  sentences = list(nlp(document['document']).sents)
-  pointers = [i for i in range(len(sentences)) if '|||||' in sentences[i].text] #seperate articles
+  percentage = 0.7 #percentage to keep
+  top = 3 # keep first t sentences 
+  sentences = document['document']
+  pointers = [i for i in range(len(sentences)) if '|||||' in sentences[i]] #seperate articles
   scores = document['rouge_scores']
   filter = []
   score_splits = [sl.tolist()for sl in np.split(scores, pointers)]
   for a in range(len(score_splits)):
-    rest = score_splits[a][5:]
+    rest = score_splits[a][top:]
     n_leave = int(len(rest)*percentage) #leave 0.8 percent of the rest of the sentences (except top sentences)
     index_leave = sorted(range(len(rest)), key = lambda sub: rest[sub])[-n_leave:] #index of top n_leave scores
     if len(score_splits[a])>=top:
@@ -26,12 +26,11 @@ def filter_sentence(document):
       filter.append([1]*(len(score_splits[a])))
     for i in index_leave:
       filter[a][i+top] = 1
-  filter = sum(filter,[])
-  filtered_sentetnces = []
-  for s in range(len(sentences)):
-    if filter[s] == 1:
-      filtered_sentetnces.append(str(sentences[s]))
+  filter = list(map(bool,sum(filter,[])))
+  filtered_sentetnces = np.array(sentences)[filter]
+  filtered_scores = np.array(np.array(scores))[filter]
   document['document'] = filtered_sentetnces
+  document['rouge_scores'] = filtered_scores
   return document
 
 
